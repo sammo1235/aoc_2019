44214fcb654dc8f32be789297c39367b758d7922
sammo1235 <samrhysperry93@hotmail.co.uk>
2021-01-16 23:21:05 +0000

    Add day 4 part 2

--- lib/4/secure_container.rb
+++ lib/4/secure_container.rb
@@ -13,10 +13,36 @@ class SecureContainer
     c
   end
 
+  def part_two
+    c = 0
+    ir.each do |pw|
+      c += 1 if has_two_but_not_three?(pw) && never_decrease?(pw)
+    end
+    c
+  end
+
   def has_two_adj_ints?(pw)
     str = pw.to_s
     (1..(pw.digits.size-2)).each do |i|
       return true if str[i] == str[i-1] || str[i] == str[i+1]
+    end
+    false
+  end
+
+  def has_two_but_not_three?(pw)
+    str = pw.to_s
+    (1..(pw.digits.size-2)).each do |i|
+      if str[i] == str[i-1] && str[i] != str[i+1]
+        if i == 1
+          return true
+        else
+          if str[i] != str[i-2]
+            return true
+          end
+        end
+      end
+
+      return true if str[i] == str[i+1] && str[i] != str[i-1] && i == 4
     end
     false
   end
--- spec/4/secure_container_spec.rb
+++ spec/4/secure_container_spec.rb
@@ -2,12 +2,20 @@ require './lib/4/secure_container'
 
 RSpec.describe SecureContainer do
   let(:test_range) { described_class.new([111111, 223450, 123789])}
+  let(:test_range_2) { described_class.new([112233, 123444, 111122])}
   let(:actual_range) { described_class.new((265275..781584))}
 
   describe '#part_one' do
     it 'gives number of valid passwords in range' do
       expect(test_range.part_one).to eq(1)
       expect(actual_range.part_one).to eq(960)
+    end
+  end
+
+  describe '#part_two' do
+    it 'gives valid passwords with new rule (has to have only 2 adj ints)' do
+      expect(test_range_2.part_two).to eq(2)
+      expect(actual_range.part_two).to eq(626)
     end
   end
 end
