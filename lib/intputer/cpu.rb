740b6a37d7884daaa1723ad7a7d259662a3cc3be
sammo1235 <samrhysperry93@hotmail.co.uk>
2021-01-19 19:44:13 +0000

    Refactor intputer

--- lib/intputer/cpu.rb
+++ lib/intputer/cpu.rb
@@ -1,91 +1,72 @@
 require 'byebug'
 
 class Cpu
-  attr_accessor :input
+  attr_accessor :ind, :input, :params
 
   def initialize(input)
     @input = input
+    @ind = 0
+    @params = {}
   end
 
 
   def compute(parameter_mode: false, diagnostic_mode: false)
-    index = 0
     while true
       major_skip = true
-
-      params = {}
-
-      opcode = input[index]
+      opcode = input[ind]
+      store_position = input[ind+3]
 
       if diagnostic_mode
-        return input if input[index+1].nil? || opcode.nil? || opcode == 99
+        return input if input[ind+1].nil? || opcode.nil? || opcode == 99
       end
 
-      begin
-        # parameter_mode
-        if parameter_mode && opcode.digits.size > 1
-          param_codes = opcode.digits[2..-1].map(&:zero?) # 0 == position mode
-          opcode = opcode.digits.first
-          while param_codes.size < 3
-            param_codes << true
-          end
-          (1..2).each do |position|
-            if param_codes[position-1] # position mode
-              params[position.to_s] = input[input[index+position]]
-            else # immediate_mode
-              params[position.to_s] = input[index+position]
-            end
-          end
-          store_position = input[index+3]
-        elsif [3, 4].include? opcode
-          # params are handled below if code is 3 or 4
-        else
-          (1..2).each do |pos| # all positional
-            params[pos.to_s] = input[input[index+pos]]
-          end
-          store_position = input[index+3]
-        end
-      rescue => exception
-        puts exception
-        debugger
-        break
-      end
-
-      # debugger
-      if opcode == 1
-        val = params["1"] + params["2"]
-        input[store_position] = val
-      elsif opcode == 2
-        val = params["1"] * params["2"]
-        input[store_position] = val
-      elsif opcode == 3
-        input[input[index+1]] = 1
-        major_skip = false
-      elsif opcode == 4
-        # this should always output zero until the final diagnostic code
-        output = input[input[index+1]]
-        return output if output > 3
-        major_skip = false
-      elsif opcode == 99
+      if opcode == 99
         return input[0]
-      else # catch other opcodes
-        # do nothing
       end
-      if major_skip
-        index += 4
+
+      if parameter_mode && opcode > 4
+        parameter_interpreter(ind, true, opcode)
+        opcode = opcode.digits.first
       else
-        index += 2
+        parameter_interpreter(ind, false)
       end
+
+      run_opcode(opcode, store_position)
+
+      major_skip = false if [3, 4].include? opcode
+      major_skip ? self.ind += 4 : self.ind += 2
     end
   end
 
-  def parameter_iterpreter(opcode)
-
+  def run_opcode(opcode, store_position)
+    case opcode
+    when 1
+      input[store_position] = params["1"] + params["2"]
+    when 2
+      input[store_position] = params["1"] * params["2"]
+    when 3
+      input[input[ind+1]] = 1
+    when 4
+      exit(0) if input[input[ind+1]] > 3
+    end
   end
-end
 
-class OpcodeInterpreter
-  def initialize(opcode)
+  def parameter_interpreter(index, parameter_mode = true, opcode = nil)
+    param_codes = if parameter_mode
+      opcode.digits[2..-1].map(&:zero?)
+    else
+      [true]
+    end
 
+    while param_codes.size < 3
+      param_codes << true
+    end
+    (1..2).each do |position|
+      if param_codes[position-1] # position mode
+        params[position.to_s] = input[input[index+position]]
+      else # immediate_mode
+        params[position.to_s] = input[index+position]
+      end
+    end
   end
 end
