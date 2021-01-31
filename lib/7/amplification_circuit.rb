20c70d615f76ac39db4eb561a5b219f2f90a04c1
sammo1235 <samrhysperry93@hotmail.co.uk>
2021-01-31 11:36:46 +0000

    refactor amplification circuits

--- lib/7/amplification_circuit.rb
+++ lib/7/amplification_circuit.rb
@@ -1,101 +1,71 @@
 require './lib/intputer/cpu'
 
 class AmplificationCircuit
-  attr_accessor :file, :phase_setting_sequence
+  attr_accessor :file, :phase_setting_sequence, :final_signals
 
   def initialize(file, phase_setting_sequence = nil)
     @file = file
     @phase_setting_sequence = phase_setting_sequence
+    @final_signals = []
   end
 
   def part_one
     input_signal = 0
     if phase_setting_sequence
-      phase_setting_sequence.each do |phase_setting|
-        output = Cpu.new(get_input, false, input_signal, phase_setting).compute
-        input_signal = output
-      end
+      run_sequential_phase_permutation(phase_setting_sequence)
     else
-      # find maximum signal that can be sent to thrusters
-      final_signals = []
       (0..4).to_a.permutation.each do |phase_setting_sequence|
-        input_signal = 0
-        phase_setting_sequence.each_with_index do |phase_setting, index|
-          output = Cpu.new(get_input, false, input_signal, phase_setting).compute
-          if index < 4
-            input_signal = output
-          else
-            final_signals << output
-          end
-        end
+        run_sequential_phase_permutation(phase_setting_sequence)
       end
     end
-    if final_signals
-      final_signals.max
-    else
-      input_signal
+
+    final_signals.max
+  end
+
+  def run_sequential_phase_permutation(phase_setting_sequence)
+    input_signal = 0
+    phase_setting_sequence.each_with_index do |phase_setting, index|
+      output = Cpu.new(get_input, false, input_signal, phase_setting).compute
+      if index < 4
+        input_signal = output
+      else
+        final_signals << output
+      end
     end
   end
 
   def part_two
-    amps = {}
-    input_signal = 0
-
-    if phase_setting_sequence # for diagnosis
-      first_run = true
-      while true
-        phase_setting_sequence.each_with_index do |phase_setting, index|
-          amp = amps[index.to_s] || Cpu.new(get_input, false, input_signal, phase_setting)
-          amp.ind += 2 unless first_run # continue the program
-          amp.quantum_fluctuating_input = input_signal
-          amp.phase_setting = nil unless first_run
-
-          output = amp.compute
-
-          if output.is_a?(Array)
-            return input_signal
-          else
-            if output > input_signal
-              input_signal = output
-            end
-          end
-          amps[index.to_s] = amp
-        end
-        first_run = false
-      end
-    else # run actual data
-      final_signals = []
+    if phase_setting_sequence
+      run_concurrent_phase_permutation(phase_setting_sequence)
+    else
       (5..9).to_a.permutation.each do |phase_setting_sequence|
-        first_run = true
-        amps = {}
-        input_signal = 0
-        do_break = false
-        while true
-          phase_setting_sequence.each_with_index do |phase_setting, index|
-            amp = amps[index.to_s] || Cpu.new(get_input, false, input_signal, phase_setting)
-            amp.ind += 2 unless first_run # continue the program
-            amp.quantum_fluctuating_input = input_signal
-            amp.phase_setting = nil unless first_run
-
-            output = amp.compute
-
-            if output.is_a? Array
-              final_signals << input_signal
-              do_break = true
-              break
-            else
-              if output > input_signal
-                input_signal = output
-              end
-            end
-            amps[index.to_s] = amp
-          end
-          break if do_break
-          first_run = false
-        end
+        run_concurrent_phase_permutation(phase_setting_sequence)
       end
     end
+
     final_signals.max
+  end
+
+  def run_concurrent_phase_permutation(phase_setting_sequence)
+    amps = {}
+    input_signal = 0
+    while true
+      phase_setting_sequence.each_with_index do |phase_setting, index|
+        amp = amps[index.to_s] || Cpu.new(get_input, false, input_signal, phase_setting)
+        amp.ind += 2 if amp.phase_setting.nil? # continue the program
+        amp.quantum_fluctuating_input = input_signal
+
+        output = amp.compute
+
+        if output.is_a?(Array)
+          final_signals << input_signal
+          return
+        else
+          input_signal = output
+        end
+        amps[index.to_s] = amp
+      end
+    end
   end
 
   def get_input
